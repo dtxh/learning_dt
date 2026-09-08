f49bd2df380e489da662deb54281d946b1c50130
微风の奶龙 <2921735919@qq.com>
2026-09-08 17:53:02 +0800

    2.0

--- cpp/CircularQueue.cpp
+++ cpp/CircularQueue.cpp
@@ -9,9 +9,9 @@ typedef struct CircularQueue {
     int data[MAXSIZE];
 } CircularQueue;
 
-bool QueueInit(CircularQueue* cq) {
-    cq->front = 0;
-    cq->rear = 0;
+bool QueueInit(CircularQueue &cq) {
+    cq.front = 0;
+    cq.rear = 0;
     return true;
 }
 
@@ -29,21 +29,21 @@ bool IsEmpty(CircularQueue cq) {
     return false;
 }
 
-bool EnQueue(CircularQueue* cq,int x) {
-    if(IsFull(*cq)) {
+bool EnQueue(CircularQueue &cq,int x) {
+    if(IsFull(cq)) {
         return false;
     }
-    cq->data[cq->rear] = x;
-    cq->rear = (cq->rear+1)%MAXSIZE;
+    cq.data[cq.rear] = x;
+    cq.rear = (cq.rear+1)%MAXSIZE;
     return true;
 }
 
-bool DeQueue(CircularQueue* cq,int &x) {
-    if(IsEmpty(*cq)) {
+bool DeQueue(CircularQueue &cq,int &x) {
+    if(IsEmpty(cq)) {
         return false;
     }
-    x = cq->data[cq->front];
-    cq->front = (cq->front+1)%MAXSIZE;
+    x = cq.data[cq.front];
+    cq.front = (cq.front+1)%MAXSIZE;
     return true;
 }
 
@@ -52,27 +52,29 @@ bool QueueGetFront(CircularQueue cq,int &x) {
         return false;
     }
     x = cq.data[cq.front];
+    cq.front = (cq.front+1)%MAXSIZE;
+    cq.front = (cq.front+1)%MAXSIZE;
     return true;
 }
 
-bool ClearQueue(CircularQueue* cq) {
-    cq->front = 0;
-    cq->rear = 0;
+bool ClearQueue(CircularQueue &cq) {
+    cq.front = 0;
+    cq.rear = 0;
     return true;
 }
 
 int main() {
     CircularQueue cq;
-    QueueInit(&cq);
-    EnQueue(&cq,1);
-    EnQueue(&cq,2);
-    EnQueue(&cq,3);
+    QueueInit(cq);
+    EnQueue(cq,1);
+    EnQueue(cq,2);
+    EnQueue(cq,3);
     int x = 0;
-    DeQueue(&cq,x);
-    DeQueue(&cq,x);
-    EnQueue(&cq,4);
-    EnQueue(&cq,5);
-    EnQueue(&cq,6);
+    DeQueue(cq,x);
+    DeQueue(cq,x);
+    EnQueue(cq,4);
+    EnQueue(cq,5);
+    EnQueue(cq,6);
     int i = cq.front;
     while(i != cq.rear) {
         cout <<cq.data[i]<<" ";
